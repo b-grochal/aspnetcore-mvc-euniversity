15de730e63a711eadd665608d374e2d3211cb31c
Bartosz <radu480@o2.pl>
2021-11-06 00:56:07 +0100

    Create interfaces for repositories of domain entities.

---
+++ eUniversity.Application/Contracts/Persistence/ICourseRepository.cs
@@ -0,0 +1,11 @@
+﻿using eUniversity.Domain.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eUniversity.Application.Contracts.Persistence
+{
+    public interface ICourseRepository : IAsyncRepository<Course>
+    {
+    }
+}
