baf249ec54f99f4638c7e13dc90f4f1ed08c5c7d
Bartosz <radu480@o2.pl>
2021-11-07 00:30:46 +0100

    Create logic for deleting admin.

---
+++ eUniversity.Application/Functions/Admins/Commands/DeleteAdmin/DeleteAdminCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Admins.Commands.DeleteAdmin
+{
+    public class DeleteAdminCommandHandler : IRequestHandler<DeleteAdminCommand>
+    {
+        private readonly IAdminRepository _adminRepository;
+
+        public DeleteAdminCommandHandler(IAdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+        }   
+
+        public async Task<Unit> Handle(DeleteAdminCommand request, CancellationToken cancellationToken)
+        {
+            var adminToDelete = await _adminRepository.GetByIdAsync(request.AdminId);
+
+            await _adminRepository.DeleteAsync(adminToDelete);
+
+            return Unit.Value;
+        }
+    }
+}
