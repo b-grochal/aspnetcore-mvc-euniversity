a8600ac28b10ae81e9752812033ece9f2a3f13da
Bartosz <radu480@o2.pl>
2021-11-07 00:45:31 +0100

    Create logic for fetching admin's details.

---
+++ eUniversity.Application/Functions/Admins/Queries/GetAdminDetail/AdminDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eUniversity.Application.Functions.Admins.Queries.GetAdminDetail
+{
+    public class AdminDetailViewModel
+    {
+        public string AdminId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
