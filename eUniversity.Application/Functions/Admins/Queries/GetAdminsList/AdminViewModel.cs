b3d7b758ae68e382cccf004b2279499ab1b2a6f8
Bartosz <radu480@o2.pl>
2021-11-07 00:56:55 +0100

    Create logic for fetching list of all admins.

---
+++ eUniversity.Application/Functions/Admins/Queries/GetAdminsList/AdminViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eUniversity.Application.Functions.Admins.Queries.GetAdminsList
+{
+    public class AdminViewModel
+    {
+    }
+}
---
+++ eUniversity.Application/Functions/Admins/Queries/GetAdminsList/GetAdminsListQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Admins.Queries.GetAdminsList
+{
+    public class GetAdminsListQueryHandler : IRequestHandler<GetAdminsListQuery, List<AdminViewModel>>
+    {
+        private readonly IAdminRepository _adminRepository;
+        private readonly IMapper _mapper;
+
+        public GetAdminsListQueryHandler(IAdminRepository adminRepository, IMapper mapper)
+        {
+            _adminRepository = adminRepository;
+            _mapper = mapper;
+        }
+
+        public Task<List<AdminViewModel>> Handle(GetAdminsListQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
