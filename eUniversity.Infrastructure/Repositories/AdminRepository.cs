d3899c641e2a97a465a050e2b1f8b83e05d40e24
Bartosz <radu480@o2.pl>
2021-11-10 20:00:55 +0100

    Created empty repository for admins.

---
+++ eUniversity.Infrastructure/Repositories/AdminRepository.cs
@@ -0,0 +1,47 @@
+﻿using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using eUniversity.Domain.Enitities;
+using eUniversity.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Infrastructure.Repositories
+{
+    public class AdminRepository : IAdminRepository
+    {
+        private readonly UserManager<IdentityAdmin> _userManager;
+
+        public AdminRepository(UserManager<IdentityAdmin> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task<Admin> AddAsync(Admin entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Admin entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Admin>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Admin> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Admin entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
