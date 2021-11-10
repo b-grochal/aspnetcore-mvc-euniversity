5b11126ce51d41a71e9d7456aa937b451099b082
Bartosz <radu480@o2.pl>
2021-11-10 21:09:27 +0100

    Prepare installer for infrastructure layer.

---
+++ eUniversity.Infrastructure/InfrastructureInstallation.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using eUniversity.Infrastructure.Repositories;
+
+namespace eUniversity.Infrastructure
+{
+    public static class InfrastructureInstallation
+    {
+        public static IServiceCollection AddEUniversityInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EUniversityContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("EUniversityConnectionString")));
+
+            services.AddScoped<IAdminRepository, AdminRepository>();
+
+            return services;
+        }
+    }
+}
