9a217123de51c32b0bfa78b1fab5dbfb514a4221
Bartosz <radu480@o2.pl>
2021-11-24 00:44:34 +0100

    Prepare dummy data for degrees.

---
+++ eUniversity.Infrastructure/DummyData/DummyDegrees.cs
@@ -0,0 +1,31 @@
+﻿using eUniversity.Domain.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Infrastructure.DummyData
+{
+    public class DummyDegrees
+    {
+        public static List<Degree> Get()
+        {
+            return new List<Degree>
+            {
+                new Degree
+                {
+                    Name = "Bachelor's degree"
+                },
+                new Degree
+                {
+                    Name = "Engineer's degree"
+                },
+                new Degree
+                {
+                    Name = "Master's degree"
+                }
+            };
+        }
+    }
+}
---
+++ eUniversity.Infrastructure/DummyData/DummySeed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Infrastructure.DummyData
+{
+    class DummySeed
+    {
+        public static int ComputerScience = 1;
+        public static int Biotechnology = 2;
+        public static int Physics = 3;
+        public static int Mathematics = 4;
+
+        public static int NoGrade = 1;
+        public static int Grade2 = 2;
+        public static int Grade3 = 3;
+        public static int Grade4 = 4;
+        public static int Grade5 = 5;
+    }
+}
