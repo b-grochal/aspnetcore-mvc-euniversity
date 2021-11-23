1cee351cb636b5b3d31b17cde53cde6fb6ac6aef
Bartosz <radu480@o2.pl>
2021-11-24 00:18:53 +0100

    Prepare dummy data for subjects.

---
+++ eUniversity.Infrastructure/DummyData/DummySubjects.cs
@@ -0,0 +1,34 @@
+﻿using eUniversity.Domain.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Infrastructure.DummyData
+{
+    public class DummySubjects
+    {
+        public static List<Subject> Get()
+        {
+            return new List<Subject> {
+                new Subject
+                {
+                    Name = "Computer Science"
+                },
+                new Subject
+                {
+                    Name = "Biotechnology"
+                },
+                new Subject
+                {
+                    Name = "Physics"
+                },
+                new Subject
+                {
+                    Name = "Mathematics"
+                }
+            };
+        }
+    }
+}
