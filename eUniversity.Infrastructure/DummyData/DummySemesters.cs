d06e3f079cfc77092648b04358528e10a3b7cfea
Bartosz <radu480@o2.pl>
2021-11-24 00:56:27 +0100

    Prepare dummy semesters.

---
+++ eUniversity.Infrastructure/DummyData/DummySemesters.cs
@@ -0,0 +1,59 @@
+﻿using eUniversity.Domain.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Infrastructure.DummyData
+{
+    public class DummySemesters
+    {
+        public static List<Semester> Get()
+        {
+            return new List<Semester>
+            {
+                new Semester
+                {
+                    Name = "First"
+                },
+                new Semester
+                {
+                    Name = "Second"
+                },
+                new Semester
+                {
+                    Name = "Third"
+                },
+                new Semester
+                {
+                    Name = "Fourth"
+                },
+                new Semester
+                {
+                    Name = "Fifth"
+                },
+                new Semester
+                {
+                    Name = "Sixth"
+                },
+                new Semester
+                {
+                    Name = "Seventh"
+                },
+                new Semester
+                {
+                    Name = "Eighth"
+                },
+                new Semester
+                {
+                    Name = "Ninth"
+                },
+                new Semester
+                {
+                    Name = "Tenth"
+                }
+            };
+        }
+    }
+}
