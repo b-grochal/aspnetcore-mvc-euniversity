bcf9604bd537640cde0fcc3ebb462f4039df6ef7
Bartosz <radu480@o2.pl>
2021-11-24 00:24:26 +0100

    Prepare dummy data for grades.

---
+++ eUniversity.Infrastructure/DummyData/DummyGrades.cs
@@ -0,0 +1,39 @@
+﻿using eUniversity.Domain.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Infrastructure.DummyData
+{
+    public class DummyGrades
+    {
+        public static List<Grade> Get()
+        {
+            return new List<Grade>
+            {
+                new Grade
+                {
+                    Name = "No grade"
+                },
+                new Grade
+                {
+                    Name = "2"
+                },
+                new Grade
+                {
+                    Name = "3"
+                },
+                new Grade
+                {
+                    Name = "4"
+                },
+                new Grade
+                {
+                    Name = "5"
+                },
+            };
+        }
+    }
+}
