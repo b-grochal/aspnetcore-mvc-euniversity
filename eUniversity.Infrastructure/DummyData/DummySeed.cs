805832c90408616771a8bb9564d0f08082356149
Bartosz <radu480@o2.pl>
2021-11-24 01:04:21 +0100

    Create dummy identifiers for dummy data.

--- eUniversity.Infrastructure/DummyData/DummySeed.cs
+++ eUniversity.Infrastructure/DummyData/DummySeed.cs
@@ -18,5 +18,20 @@ namespace eUniversity.Infrastructure.DummyData
         public static int Grade3 = 3;
         public static int Grade4 = 4;
         public static int Grade5 = 5;
+
+        public static int BachelorsDegree = 1;
+        public static int EngineersDegree = 2;
+        public static int MastersDegree = 3;
+
+        public static int FirstSemester = 1;
+        public static int SecondSemester = 2;
+        public static int ThirdSemester = 3;
+        public static int FourthSemester = 4;
+        public static int FifthSemester = 5;
+        public static int SixthSemester = 6;
+        public static int SeventhSemester = 7;
+        public static int EighthSemester = 8;
+        public static int NinthSemester = 9;
+        public static int TenthSemester = 10;
     }
 }
