26d7ce81ddce9fbdbcbf4bdcf1489ab45e48bedf
Bartosz <radu480@o2.pl>
2021-11-29 23:48:05 +0100

    Create login page layout.

---
+++ eUniversity.WebUI/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using eUniversity.Application.Functions.Auth.Commands.Login;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eUniversity.WebUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginCommand loginCommand)
+        {
+            ModelState.AddModelError("Test", "test");
+            return View();
+        }
+    }
+}
