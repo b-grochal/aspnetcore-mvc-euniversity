611d9cc0446ee06ad44c9217d2bb688bb07866dd
Bartosz <radu480@o2.pl>
2021-11-30 20:08:14 +0100

    Create details view with viewmodel.

---
+++ eUniversity.WebUI/Models/Admins/AdminDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eUniversity.WebUI.Models.Admins
+{
+    public class AdminDetailsViewModel
+    {
+        [DisplayName("Identifier")]
+        public int AdminId { get; set; }
+
+        [DisplayName("First name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Username")]
+        public string Username { get; set; }
+
+        [DisplayName("Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
