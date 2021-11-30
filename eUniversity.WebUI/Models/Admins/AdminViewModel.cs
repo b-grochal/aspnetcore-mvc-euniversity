36cfd6beda00f026a85b8025baab2680e2f872a2
Bartosz <radu480@o2.pl>
2021-11-30 20:30:51 +0100

    Create delete view with viewmodels.

---
+++ eUniversity.WebUI/Models/Admins/AdminViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eUniversity.WebUI.Models.Admins
+{
+    public class AdminViewModel
+    {
+        public int AdminId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+    }
+}
