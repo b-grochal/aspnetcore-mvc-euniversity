b493528338f4bfb6e17e1c246bbffd2af99359a1
Bartosz <radu480@o2.pl>
2021-12-12 00:50:18 +0100

    Create query for fetching students.

---
+++ eUniversity.Application/Functions/Students/Queries/GetStudentsList/StudentsListDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Students.Queries.GetStudentsList
+{
+    public class StudentsListDto
+    {
+        public List<StudentDto> Students { get; set; }
+        public string SearchedUsername { get; set; }
+    }
+}
