f18722c904e5e81c5e9411878771775a88aebb7e
Bartosz <radu480@o2.pl>
2021-12-12 00:35:39 +0100

    Create command for deleting student.

---
+++ eUniversity.Application/Functions/Students/Commands/DeleteStudent/DeleteStudentCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Students.Commands.DeleteStudent
+{
+    public class DeleteStudentCommandHandler : IRequestHandler<DeleteStudentCommand>
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public DeleteStudentCommandHandler(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteStudentCommand request, CancellationToken cancellationToken)
+        {
+            var adminToDelete = await _studentRepository.GetByIdAsync(request.StudentId);
+
+            await _studentRepository.DeleteAsync(adminToDelete);
+
+            return Unit.Value;
+        }
+    }
+}
