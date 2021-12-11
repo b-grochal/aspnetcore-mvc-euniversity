8fc6b9444d5f7252209146a7cfb42aacc931ed73
Bartosz <radu480@o2.pl>
2021-12-12 00:38:51 +0100

    Create command for updating students.

---
+++ eUniversity.Application/Functions/Students/Commands/UpdateStudent/UpdateStudentCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using eUniversity.Domain.Enitities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Students.Commands.UpdateStudent
+{
+    class UpdateStudentCommandHandler : IRequestHandler<UpdateStudentCommand>
+    {
+        private readonly IStudentRepository _studentRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateStudentCommandHandler(IStudentRepository studentRepository, IMapper mapper)
+        {
+            _studentRepository = studentRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateStudentCommand request, CancellationToken cancellationToken)
+        {
+            var admin = _mapper.Map<Student>(request);
+
+            await _studentRepository.UpdateAsync(admin);
+
+            return Unit.Value;
+        }
+    }
+}
