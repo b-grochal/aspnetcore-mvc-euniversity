4489574f6f55f67dde80c505c430c3d8f139d696
Bartosz <radu480@o2.pl>
2021-12-11 23:49:44 +0100

    Create classes for policy based authorization.

---
+++ eUniversity.WebUI/Authorization/IsStudentHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eUniversity.WebUI.Authorization
+{
+    public class IsStudentHandler : AuthorizationHandler<IsStudentRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsStudentRequirement requirement)
+        {
+            if (!context.User.IsInRole("Student"))
+            {
+                context.Fail();
+            }
+            else
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
