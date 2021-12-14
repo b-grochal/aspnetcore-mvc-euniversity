3d7f032df23bc2b752c0cd43f2c6cf72e68d3755
Bartosz <radu480@o2.pl>
2021-12-14 17:35:02 +0100

    Implement create command for course.

---
+++ eUniversity.Application/Functions/Courses/Commands/CreateCourse/CreateCourseCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using eUniversity.Domain.Enitities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Courses.Commands.CreateCourse
+{
+    public class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, CreateCourseCommandResponse>
+    {
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMapper _mapper;
+
+        public CreateCourseCommandHandler(ICourseRepository courseRepository, IMapper mapper)
+        {
+            _courseRepository = courseRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CreateCourseCommandResponse> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateCourseCommandValidator();
+            var validatorResult = await validator.ValidateAsync(request);
+
+            if (!validatorResult.IsValid)
+                return new CreateCourseCommandResponse(validatorResult);
+
+            var course = _mapper.Map<Course>(request);
+            course = await _courseRepository.AddAsync(course);
+
+            return new CreateCourseCommandResponse(course.CourseId);
+        }
+    }
+}
---
+++ eUniversity.Application/Functions/Courses/Commands/CreateCourse/CreateCourseCommandValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Courses.Commands.CreateCourse
+{
+    public class CreateCourseCommandValidator : AbstractValidator<CreateCourseCommand>
+    {
+        public CreateCourseCommandValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .WithMessage("{PorpertyName} should be not empty.");
+
+            RuleFor(c => c.Password)
+                .NotEmpty()
+                .WithMessage("{PorpertyName} should be not empty.");
+
+            RuleFor(c => c.ConfirmationPassword)
+                .Equal(c => c.Password)
+                .WithMessage("Password and confirmation password must match each other.");
+
+            RuleFor(c => c.DegreeId)
+                .GreaterThan(0)
+                .WithMessage("{PorpertyName} as foreign key should be greater than zero.");
+
+            RuleFor(c => c.SemesterId)
+                .GreaterThan(0)
+                .WithMessage("{PorpertyName} as foreign key should be greater than zero.");
+
+            RuleFor(c => c.SubjectId)
+                .GreaterThan(0)
+                .WithMessage("{PorpertyName} as foreign key should be greater than zero.");
+        }
+    }
+}
