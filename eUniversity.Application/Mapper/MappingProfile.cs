4581e64c629c1eece62bbc11e5f3d10014eed76b
Bartosz <radu480@o2.pl>
2021-12-14 16:52:04 +0100

    Prepare mapping profiles for teachers and students.

--- eUniversity.Application/Mapper/MappingProfile.cs
+++ eUniversity.Application/Mapper/MappingProfile.cs
@@ -3,6 +3,14 @@ using eUniversity.Application.Functions.Admins.Commands.CreateAdmin;
 using eUniversity.Application.Functions.Admins.Commands.UpdateAdmin;
 using eUniversity.Application.Functions.Admins.Queries.GetAdminDetail;
 using eUniversity.Application.Functions.Admins.Queries.GetAdminsList;
+using eUniversity.Application.Functions.Students.Commands.CreateStudent;
+using eUniversity.Application.Functions.Students.Commands.UpdateStudent;
+using eUniversity.Application.Functions.Students.Queries.GetStudentDetails;
+using eUniversity.Application.Functions.Students.Queries.GetStudentsList;
+using eUniversity.Application.Functions.Teachers.Commands.CreateTeacher;
+using eUniversity.Application.Functions.Teachers.Commands.UpdateTeacher;
+using eUniversity.Application.Functions.Teachers.Queries.GetTeacherDetails;
+using eUniversity.Application.Functions.Teachers.Queries.GetTeachersList;
 using eUniversity.Domain.Enitities;
 using System;
 using System.Collections.Generic;
@@ -14,6 +22,7 @@ namespace eUniversity.Application.Mapper
     {
         public MappingProfile()
         {
+            // Admins
             CreateMap<CreateAdminCommand, Admin>()
                 .ReverseMap();
 
@@ -24,6 +33,32 @@ namespace eUniversity.Application.Mapper
                 .ReverseMap();
 
             CreateMap<Admin, AdminDto>()
+                .ReverseMap();
+
+            // Students
+            CreateMap<CreateStudentCommand, Student>()
+                .ReverseMap();
+
+            CreateMap<UpdateStudentCommand, Student>()
+                .ReverseMap();
+
+            CreateMap<Student, StudentDetailsDto>()
+                .ReverseMap();
+
+            CreateMap<Student, StudentDto>()
+                .ReverseMap();
+
+            // Teachers
+            CreateMap<CreateTeacherCommand, Teacher>()
+                .ReverseMap();
+
+            CreateMap<UpdateTeacherCommand, Teacher>()
+                .ReverseMap();
+
+            CreateMap<Teacher, TeacherDetailsDto>()
+                .ReverseMap();
+
+            CreateMap<Teacher, TeacherDto>()
                 .ReverseMap();
         }
     }
