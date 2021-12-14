972f6c1473fd78b8ec6ee90cd9a88ca06539340e
Bartosz <radu480@o2.pl>
2021-12-14 17:38:43 +0100

    Implement delete command for course.

---
+++ eUniversity.Application/Functions/Courses/Commands/DeleteCourse/DeleteCourseCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Courses.Commands.DeleteCourse
+{
+    public class DeleteCourseCommand : IRequest
+    {
+        public int CourseId { get; set; }
+    }
+}
---
+++ eUniversity.Application/Functions/Courses/Commands/DeleteCourse/DeleteCourseCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Courses.Commands.DeleteCourse
+{
+    public class DeleteCourseCommandHandler : IRequestHandler<DeleteCourseCommand>
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public DeleteCourseCommandHandler(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteCourseCommand request, CancellationToken cancellationToken)
+        {
+            var courseToDelete = await _courseRepository.GetByIdAsync(request.CourseId);
+
+            await _courseRepository.DeleteAsync(courseToDelete);
+
+            return Unit.Value;
+        }
+    }
+}
