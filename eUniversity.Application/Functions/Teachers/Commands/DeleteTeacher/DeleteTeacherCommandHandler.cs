9bf33e3f6967c6ed6ed4140a56fff6a5a2177ea8
Bartosz <radu480@o2.pl>
2021-12-14 15:50:26 +0100

    Implement command for deleting teachers.

---
+++ eUniversity.Application/Functions/Teachers/Commands/DeleteTeacher/DeleteTeacherCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Teachers.Commands.DeleteTeacher
+{
+    public class DeleteTeacherCommandHandler : IRequestHandler<DeleteTeacherCommand>
+    {
+        private readonly ITeacherRepository _teacherRepository;
+
+        public DeleteTeacherCommandHandler(ITeacherRepository teacherRepository)
+        {
+            _teacherRepository = teacherRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteTeacherCommand request, CancellationToken cancellationToken)
+        {
+            var teacherToDelete = await _teacherRepository.GetByIdAsync(request.TeacherId);
+
+            await _teacherRepository.DeleteAsync(teacherToDelete);
+
+            return Unit.Value;
+        }
+    }
+}
