cc6bc4212404c53a60ce8a8634df67c7b302d327
Bartosz <radu480@o2.pl>
2021-12-14 16:42:34 +0100

    Implement query for fetching list of teachers.

---
+++ eUniversity.Application/Functions/Teachers/Queries/GetTeachersList/GetTeachersListQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Teachers.Queries.GetTeachersList
+{
+    public class GetTeachersListQueryHandler : IRequestHandler<GetTeachersListQuery, TeachersListDto>
+    {
+        private readonly ITeacherRepository _teacherRepository;
+        private readonly IMapper _mapper;
+
+        public GetTeachersListQueryHandler(ITeacherRepository teacherRepository, IMapper mapper)
+        {
+            _teacherRepository = teacherRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<TeachersListDto> Handle(GetTeachersListQuery request, CancellationToken cancellationToken)
+        {
+            var teachers = await _teacherRepository.GetAllAsync(request.SearchedUserName);
+            return new TeachersListDto
+            {
+                Teachers = _mapper.Map<List<TeacherDto>>(teachers),
+                SearchedUsername = request.SearchedUserName
+            };
+        }
+    }
+}
---
+++ eUniversity.Application/Functions/Teachers/Queries/GetTeachersList/TeachersListDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Teachers.Queries.GetTeachersList
+{
+    public class TeachersListDto
+    {
+        public List<TeacherDto> Teachers { get; set; }
+        public string SearchedUsername { get; set; }
+    }
+}
