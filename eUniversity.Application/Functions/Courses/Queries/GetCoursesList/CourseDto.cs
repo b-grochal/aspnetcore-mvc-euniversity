20847e88566199c18528055afe6af71739460dae
Bartosz <radu480@o2.pl>
2021-12-14 18:30:33 +0100

    Implement query for fetching list of courses.

---
+++ eUniversity.Application/Functions/Courses/Queries/GetCoursesList/CourseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Courses.Queries.GetCoursesList
+{
+    public class CourseDto
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public string SubjectName { get; set; }
+        public string SemesterName { get; set; }
+        public string DegreeName { get; set; }
+
+    }
+}
---
+++ eUniversity.Application/Functions/Courses/Queries/GetCoursesList/GetCoursesListQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Courses.Queries.GetCoursesList
+{
+    public class GetCoursesListQuery : IRequest<CoursesListDto>
+    {
+        public string SearchedName { get; set; }
+    }
+}
