9c09833e4c179724c1f6947d81f0e52949b5f115
Bartosz <radu480@o2.pl>
2021-12-16 23:30:35 +0100

    Prepare viewmodels for teachers.

---
+++ eUniversity.WebUI/Models/Teachers/EditTeacherViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eUniversity.WebUI.Models.Teachers
+{
+    public class EditTeacherViewModel
+    {
+        [Required]
+        public int TeacherId { get; set; }
+
+        [Required]
+        [DisplayName("First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DisplayName("Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
