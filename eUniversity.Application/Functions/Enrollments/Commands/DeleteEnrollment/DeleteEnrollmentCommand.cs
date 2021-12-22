aac609f34ef34891a785a6f392aa269c26f42d09
Bartosz <radu480@o2.pl>
2021-12-22 20:25:15 +0100

    Implement delete command for course.

---
+++ eUniversity.Application/Functions/Enrollments/Commands/DeleteEnrollment/DeleteEnrollmentCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Enrollments.Commands.DeleteEnrollment
+{
+    public class DeleteEnrollmentCommand : IRequest
+    {
+        public int EnrollmentId { get; set; }
+    }
+}
---
+++ eUniversity.Application/Functions/Enrollments/Commands/DeleteEnrollment/DeleteEnrollmentCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using eUniversity.Application.Contracts.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eUniversity.Application.Functions.Enrollments.Commands.DeleteEnrollment
+{
+    public class DeleteEnrollmentCommandHandler : IRequestHandler<DeleteEnrollmentCommand>
+    {
+        private readonly IEnrollmentRepository _enrollmentRepository;
+
+        public DeleteEnrollmentCommandHandler(IEnrollmentRepository enrollmentRepository)
+        {
+            _enrollmentRepository = enrollmentRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteEnrollmentCommand request, CancellationToken cancellationToken)
+        {
+            var enrollmentToDelete = await _enrollmentRepository.GetByIdAsync(request.EnrollmentId);
+
+            await _enrollmentRepository.DeleteAsync(enrollmentToDelete);
+
+            return Unit.Value;
+        }
+    }
+}
